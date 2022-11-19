aa01ecf5c5a12247e86381909142b9e056be3f10
Konstantin Barnaev <konstantin.barnaev@gmail.com>
2022-11-19 21:48:40 +0200

    Меню текстового редактора,англоязычная версия

--- src/main/java/com/skillfactory/Menu.java
+++ src/main/java/com/skillfactory/Menu.java
@@ -12,27 +12,27 @@ public class Menu {
         Добавьте возможность выбирать данные пункты
     */
         System.out.println ("""
-                                1. - Список документов;
-                                2. - Добавить документ;
-                                3. - Показать документ;
-                                4. - Выход.""");
+                                1. - List of documents;
+                                2. - Add document;
+                                3. - Show document;
+                                4. - Exit.""");
         DocumentProcessor dp = new DocumentProcessor();
 
 
         Scanner scan = new Scanner(System.in);
     boolean a = true;
     while (a) {
-        char userInput = scan.nextLine().charAt(0); //принимаем строку, и только одну букву берём
+        char userInput = scan.nextLine().charAt(0); //принимаем строку, и только одну первую букву берём
         switch (userInput) {
             case '1' -> dp.listDoc();
             case '2' -> dp.addDoc();
             case '3' -> dp.showDoc();
             case '4' -> {
-                System.out.println("До свидания, User");
+                System.out.println("Good bye, User");
                 scan.close();
                 a = false;
             }
-            default -> System.out.println("Неправильный выбор");
+            default -> System.out.println("Wrong selection");
         }
         }
     }
